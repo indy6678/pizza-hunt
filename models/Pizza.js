bacfdad80f21f9f44d1d95868a67adbbbed40a28
Ed <105404937+indy6678@users.noreply.github.com>
2022-09-24 17:46:29 -0500

    added comment.js

--- models/Pizza.js
+++ models/Pizza.js
@@ -16,8 +16,26 @@ const PizzaSchema = new Schema({
         type: String,
         default: 'Large'
     },
-    toppings: [] // indicates an array; will also accept "array" instead of brackets
-});
+    toppings: [], // indicates an array; will also accept "array" instead of brackets
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false // "id" is a virtual that mongoose returns, which is not needed
+}
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+})
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema)
