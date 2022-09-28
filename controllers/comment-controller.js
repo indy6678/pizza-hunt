bbcab178825cad9c59440c44a3bcdd853ee0ce2b
Ed <105404937+indy6678@users.noreply.github.com>
2022-09-27 22:01:51 -0500

    updated comment-controller.js

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -3,10 +3,10 @@ const { Pizza, Comment } = require('../models');
 const commentController = {
     // add comment to pizza
     addComment({params, body}, res) {
-        console.log(body);
+        // console.log(body);
         Comment.create(body)
         .then(({ _id}) => {
-            console.log(_id)
+            // console.log(_id)
             return Pizza.findOneAndUpdate(
                 {_id: params.pizzaId},
                 {$push: {comments: _id}}, // using $push method to add the comment's _id to an array in the specific pizza
